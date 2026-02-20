d29d6b686c80d06a745c53fcbc60092b6af7b222
Astro3OOO <fjodors.caplinskis@lvgmc.lv>
2026-02-20 11:20:09 +0200

    Edited test according to latest changes. Simplifyed simulation.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,6 @@
 from config_verification import verify_config_file
-from case_study_tool import PrepareDataSet, simulation
+from case_study_tool import simulation
+from dataset_preparation import prepare_dataset
 
 
 def test_config_verification():
@@ -16,7 +17,7 @@ def test_data_preparation():
         "pword":None
     }
     try:
-        ds = PrepareDataSet(**data_vars)
+        ds = prepare_dataset(**data_vars)
     except:
         ds = None
     assert ds is not None
@@ -26,12 +27,13 @@ def test_simulation():
         "model": "OceanDrift",
         "start_position": [57.5, 23.7],
         "start_t": "2024-06-01 00:00:00",
-        "end_t": "2024-06-03 00:00:00",
+        "end_t": "2024-06-01 06:00:00",
         "num": 1,
         "rad": 0,
         "wdf": 0.03,
-        "time_step": 1800,
+        "time_step": 3600,
         "file_name": "test_output.nc"
     }
-    o = simulation(datasets=[], **sim_vars)    
+    o, filename = simulation(datasets=[], **sim_vars)    
     assert o is not None
+
