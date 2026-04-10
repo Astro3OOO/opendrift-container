0d7ff251552cba77630f98434738d8ee2d476e34
Astro3OOO <fjodors.caplinskis@lvgmc.lv>
2026-04-10 15:30:06 +0300

    Added statistics feature to post processing. Now simulation can produce excel file with describing statistics of displacment, trajectory length and angle to the north.

--- src/post_processing/statistics.py
+++ src/post_processing/statistics.py
@@ -1,14 +1,29 @@
-import numpy as np
+import pandas as pd
+from pyproj import Geod
 
 """
     Trajectory statistics
 """
-
-def export_statistics(traj):
+def export_statistics(traj, file_name):
     
     traj_length, _, speed = traj.get_trajectory_lengths()
-    start_poz = np.array([traj.result.sel(time = traj.result.time[0].values).lon.values,
-                          traj.result.sel(time = traj.result.time[0].values).lat.values])
-    end_poz = np.array([traj.result.sel(time = traj.result.time[-1].values).lon.values,
-                          traj.result.sel(time = traj.result.time[-1].values).lat.values])
-    return
+
+    geod = Geod(ellps="WGS84")
+
+    lon1 = traj.result.lon.isel(time=0).values
+    lat1 = traj.result.lat.isel(time=0).values
+    lon2 = traj.result.lon.isel(time=-1).values
+    lat2 = traj.result.lat.isel(time=-1).values
+
+    azimuths, _, displacement = geod.inv(lon1, lat1, lon2, lat2)
+    
+    df = pd.DataFrame({
+        'Displacement (m)':displacement,
+        'Trajectory length (m)':traj_length,
+        'Angle to North (degrees)':azimuths
+        })
+    stats = df.describe()
+    
+    file_name = file_name.replace('.nc', '_statistics.xlsx') 
+    stats.to_excel(file_name)
+    return 
