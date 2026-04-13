58503c616ffbf46759dd799e55e3deb2e7e1733a
Astro3OOO <fjodors.caplinskis@lvgmc.lv>
2026-04-13 11:53:00 +0300

    Edited convex hull script to avoid loop(slow)

--- src/post_processing/convexhull.py
+++ src/post_processing/convexhull.py
@@ -1,20 +1,15 @@
 from src.post_processing.general_tools import extract_points
-from shapely.geometry import Point, MultiPoint
+from shapely import convex_hull, points, MultiPoint
 import geopandas as gpd
-from shapely import convex_hull
 
 """
     Convex hull polygon
 """
 def export_convex_hull(traj, plot_time = None):
     lats, lons = extract_points(traj, plot_time)
-    
-    points = []
-    
-    for lat, lon in zip(lats, lons):
-        points.append(Point((lon, lat)))
         
-    pol = convex_hull(MultiPoint(points))
+    geom = MultiPoint(points(lons, lats))    
+    pol = convex_hull(geom)
     
     gdf = gpd.GeoDataFrame({'geometry':[pol]}, crs="EPSG:4326")
     return gdf
